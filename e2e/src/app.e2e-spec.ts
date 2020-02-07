0654d6d74a056ad87615843083556993829ea7dc
Aaron Louie <ajlouie@gmail.com>
2020-02-07 10:22:48 -0500

    Adds some placeholder tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -12,4 +12,27 @@ describe('workspace-project App', () => {
     expect(page.getDiagramContainer()).toBeTruthy();
   });
 
+  it('should show dialog to open a diagram file');
+
+  it('should show dialog to add a new diagram file');
+
+  it('should add a new BPMN file');
+
+  it('should add a new DMN file');
+
+  it('should open a diagram file from a URL');
+
+  it('should open a local diagram file');
+
+  it('should save diagram changes');
+
+  it('should export diagram as XML file');
+
+  it('should export diagram as SVG image');
+
+  it('should save diagram changes');
+
+  it('should save diagram changes');
+
+  it('should save diagram changes');
 });
