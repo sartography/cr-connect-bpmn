36d4ab5437f8188f6424f82b44268ac20854aed4
Aaron Louie <ajlouie@gmail.com>
2020-02-08 11:39:54 -0500

    Fixes Handlebars coverage reporting bug in Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['lcovonly'],
       fixWebpackSourcePaths: true
     },
     customLaunchers: {
