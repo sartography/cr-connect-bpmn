d39e41496297c053623aabdfc064e494b4361da5
Aaron Louie <ajlouie@gmail.com>
2020-02-24 16:43:03 -0500

    Fixes broken unit tests. Updates sartography-workflow-lib.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {Component, NO_ERRORS_SCHEMA} from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {MockEnvironment} from 'sartography-workflow-lib';
 
-import { HomeComponent } from './home.component';
+import {HomeComponent} from './home.component';
+
+
+@Component({
+  selector: 'app-sign-in',
+  template: ''
+})
+class MockSignInComponent {}
+
+@Component({
+  selector: 'app-workflow-spec-list',
+  template: ''
+})
+class MockWorkflowSpecListComponent {}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +23,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        HomeComponent,
+        MockSignInComponent,
+        MockWorkflowSpecListComponent,
+      ],
     })
     .compileComponents();
   }));
@@ -21,5 +40,11 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should check signed-in state', () => {
+    const result = component.isSignedIn();
+    expect(result).toBeDefined();
+    expect(typeof result).toEqual('boolean');
   });
 });
