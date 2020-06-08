793ce28ff12a989e3b67bd4ebd49552cd382b9d8
Aaron Louie <ajlouie@gmail.com>
2020-06-07 22:22:24 -0400

    Updates sartography-workflow-lib. Adds sentry.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,6 +59,7 @@ export class ThisEnvironment implements AppEnvironment {
   irbUrl = environment.irbUrl;
   title = environment.title;
   googleAnalyticsKey = environment.googleAnalyticsKey;
+  sentryKey = environment.sentryKey;
 }
 
 /**
