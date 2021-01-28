8b39eb5098fa28c05443d7827e1d692ac62f6c88
Kelly McDonald <kelly@fammcdonald.net>
2021-01-28 10:54:36 -0500

    Make changes to tests to reflect what has been done in the UI

--- src/app/_dialogs/confirm-dialog/confirm-dialog.component.spec.ts
+++ src/app/_dialogs/confirm-dialog/confirm-dialog.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from './confirm-dialog.component';
+
 
 describe('ConfirmDialogComponent', () => {
   let component: ConfirmDialogComponent;
@@ -8,7 +9,21 @@ describe('ConfirmDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfirmDialogComponent ]
+      declarations: [ ConfirmDialogComponent ],
+      imports : [MatDialogModule],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: (dialogResult: any) => {
+            }
+          }
+        },
+        {provide: MAT_DIALOG_DATA, useValue: {
+            confirm: false,
+          }},
+      ]
+
     })
     .compileComponents();
   }));
