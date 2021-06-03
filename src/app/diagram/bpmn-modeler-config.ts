4114554b61dba69c5d209e5cb2a9cb5eb44774bf
NWalker4483 <niwal7@morgan.edu>
2021-06-03 10:31:41 -0400

    Added Script Editor

--- src/app/diagram/bpmn-modeler-config.ts
+++ src/app/diagram/bpmn-modeler-config.ts
@@ -2,6 +2,7 @@ import propertiesPanelModule from 'bpmn-js-properties-panel';
 import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
 import * as bpmnModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda.json';
 import minimapModule from 'diagram-js-minimap';
+import codeModule from 'diagram-js-code-editor';
 import {ModelerConfig} from '../_interfaces/modeler-config';
 
 export const bpmnModelerConfig: ModelerConfig = {
@@ -9,6 +10,7 @@ export const bpmnModelerConfig: ModelerConfig = {
     propertiesProviderModule,
     propertiesPanelModule,
     minimapModule,
+    codeModule,
   ],
   moddleExtensions: {
     camunda: bpmnModdleDescriptor['default']
