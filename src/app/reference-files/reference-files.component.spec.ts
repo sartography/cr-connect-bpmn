106f8d6f7593fc9c58b8868d5d109234b2fa70f1
Dan <daniel.h.funk@gmail.com>
2022-01-26 15:49:18 -0500

    Fixing a bug that prevented the download of reference files.

--- src/app/reference-files/reference-files.component.spec.ts
+++ src/app/reference-files/reference-files.component.spec.ts
@@ -111,7 +111,7 @@ describe('ReferenceFilesComponent', () => {
 
     component.openFileDialog(mockFileMetaReference0);
 
-    const fReq = httpMock.expectOne(`apiRoot/reference_file/${mockDocMeta.name}`);
+    const fReq = httpMock.expectOne(`apiRoot/reference_file/${mockDocMeta.name}/data`);
     expect(fReq.request.method).toEqual('GET');
     fReq.flush(mockArrayBuffer, {headers: mockHeaders});
 
@@ -128,7 +128,7 @@ describe('ReferenceFilesComponent', () => {
 
     component.downloadFile(mockDocMeta);
 
-    const fReq = httpMock.expectOne(`apiRoot/reference_file/${mockDocMeta.name}`);
+    const fReq = httpMock.expectOne(`apiRoot/reference_file/${mockDocMeta.name}/data`);
     expect(fReq.request.method).toEqual('GET');
     fReq.flush(mockArrayBuffer, {headers: mockHeaders});
 
